5e7e47e770c2af03a5916813358c182666d5fc90
Felipe J Colon-Gonzalez <f.colon@uea.ac.uk>
2016-12-30 14:48:47 +0000

    sent openRDS to global.R

--- shiny/app.R
+++ shiny/app.R
@@ -20,7 +20,7 @@
 ##
 ## Version 1: Initially created on 20 Dec 2016
 ##
-## Dependencies: profanity.R, pre_process.R and capstoneFunctions.R
+## Dependencies: capstoneFunctions.R
 ##
 ## Written by: Felipe J Colón-González
 ## For any problems with this code, please contact f.colon@uea.ac.uk
@@ -45,84 +45,72 @@ require(caTools)
 require(dplyr)
 
 ## -------------------------
-## Source functions and data
+## Source functions 
 ## -------------------------
 
-# Define directory tree
-myDir <- file.path("~/Documents/GitHub/capstone/")
-
 # Source functions
-source(file.path(myDir, "capstoneFunctions.R"))
-source(file.path(myDir, "profanity.R"))
-
-# Read input data
-ngram1 <- readRDS(file.path(myDir, "output", "ngram1.rds"))
-ngram2 <- readRDS(file.path(myDir, "output", "ngram2.rds"))
-ngram3 <- readRDS(file.path(myDir, "output", "ngram3.rds"))
-ngram4 <- readRDS(file.path(myDir, "output", "ngram4.rds"))
-
-
+source("capstoneFunctions.R")
 
 ## -------------------------
 ## Define UI options
 ## -------------------------
-
-ui <- dashboardPage(skin="blue",
-                    
-                    dashboardHeader(title="Word Predictor", titleWidth=250),
-                    
-                    dashboardSidebar(width=250,
-                                     numericInput("n", 
-                                                  label=h3("Maximum number of
+shinyApp(
+       ui <- dashboardPage(skin="blue",
+                           
+                           dashboardHeader(title="Word Predictor", titleWidth=250),
+                           
+                           dashboardSidebar(width=250,
+                                            numericInput("n", 
+                                                         label=h3("Maximum number of
                                                           suggestions:"), 
-                                                  value = 5,
-                                                  min = 1, 
-                                                  max = 50),
-
-                                     br(),
-                                     h5("Created by:"),
-                                     tags$a("Felipe J Colon-Gonzalez", 
-                                            href="https://github.com/FelipeJColon/capstone"),
-                                     br(),
-                                     br(),
-                                     h5("Contact me:"),
-                                     tags$a("Email", 
-                                            href="mailto:F.Colon@uea.ac.uk")),
-                    
-                    dashboardBody(
-                           fluidRow(column(12,
-                                           box(title="Enter some text",
-                                               width=10,
-                                               status="primary", 
-                                               solidHeader=TRUE,
-                                               textInput(inputId="text", 
-                                                         label="", 
-                                                         value="Your text here")))),
-                           fluidRow(
-                                  column(4,
-                                         tableOutput("table")))
-                    )
-)
-
-## -------------------------
-## Define Server options
-## -------------------------
-
-server <- shinyServer(function(input, output, session) {
+                                                         value = 1,
+                                                         min = 1, 
+                                                         max = 50),
+                                            
+                                            br(),
+                                            h5("Created by:"),
+                                            tags$a("Felipe J Colon-Gonzalez", 
+                                                   href="https://github.com/FelipeJColon/capstone"),
+                                            br(),
+                                            br(),
+                                            h5("Contact me:"),
+                                            tags$a("Email", 
+                                                   href="mailto:F.Colon@uea.ac.uk")),
+                           
+                           dashboardBody(
+                                  fluidRow(column(12,
+                                                  box(title="Enter some text",
+                                                      width=10,
+                                                      status="primary", 
+                                                      solidHeader=TRUE,
+                                                      textInput(inputId="text", 
+                                                                label="", 
+                                                                value="Your text here")))),
+                                  fluidRow(
+                                         column(4,
+                                                tableOutput("table")))
+                           )
+       ),
        
-       predWord <-  reactive( {
-              inText <- input$text
-              nWords <- input$n
-              predWord <- predictWord(inText, nRows=nWords)
+       
+       ## -------------------------
+       ## Define Server options
+       ## -------------------------
+       
+       server <- shinyServer(function(input, output, session) {
+              
+              source("global.R")
+              
+              predWord <-  reactive( {
+                     inText <- input$text
+                     nWords <- input$n
+                     predWord <- predictWord(inText, nRows=nWords)
+              })
+              
+              # Output table
+              output$table <- renderTable(predWord())
        })
-       
-       # Output table
-       output$table <- renderTable(predWord())
-})
-
-
-# Run app
-shinyApp(ui=ui, server=server)
+)
 
 # ----------------------------
 # End of file
